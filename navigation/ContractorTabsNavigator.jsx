a012e7b2da40cc807333cd80422577fafb0dbe28
Viktor <viktorg1105@gmail.com>
2025-04-24 15:15:12 +0500

    Fixed bug in ContractorTabs

--- navigation/ContractorTabsNavigator.jsx
+++ navigation/ContractorTabsNavigator.jsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons, FontAwesome5, Feather } from '@expo/vector-icons';
-import { Image } from 'react-native';
 import ContractorHomeScreen from '../screens/contractor/HomeScreen';
 import ContractorProjectsScreen from '../screens/contractor/ProjectsScreen';
 import ContractorOrdersScreen from '../screens/contractor/OrdersScreen';
